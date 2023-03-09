184954456cd866a83dbf8e8aeea848a80c3067bd
Kamen Kirilov <kamen.kirilov@conceptik.com>
2023-03-09 11:07:19 +0200

    should bump major again
    BREAKING CHANGE:  should bump major again

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer() {
     return (
         <footer>
-            Footer 3-0-1
+            Footer 3-0-1-2
         </footer>
     );
 }
--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! --change=1-0-1
+            Lerna is the original monorepo tool! --change=1-0-1-2
         </header>
     );
 }
