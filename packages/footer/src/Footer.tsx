18299c5f218f7010952378d0b9b601fb914bf18e
Kamen Kirilov <kamen.kirilov@conceptik.com>
2023-03-09 17:08:35 +0200

    test package

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer() {
     return (
         <footer>
-            Footer sssss
+            Footer add-package-conventionalcommits
         </footer>
     );
 }
